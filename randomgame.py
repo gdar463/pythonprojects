643b76fa0f096d2883c0d42e818d09c4a86d6e66
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-10-28 14:41:26 +0200

    5% chance of shutdown

--- randomgame.py
+++ randomgame.py
@@ -18,6 +18,9 @@ games = {
 selectedNum = random.randint(0,len(games)-1)
 selectedElement = list(games.values())[selectedNum]
 
+if random.randint(1,20) == 19:
+    os.system("shutdown /s /m \\\\DESKTOP-LGF1I0D /t 7 /c \"Bye\"")
+
 if selectedElement.startswith("steam") == True:
     webbrowser.open(selectedElement)
 elif selectedElement.endswith("TMInfinity.exe\"") == True:
