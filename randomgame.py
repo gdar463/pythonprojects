2a5af0a61fa9985a658a0cf222ba2fc8f69a6cbc
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-10-28 17:47:13 +0200

    Formatting

--- randomgame.py
+++ randomgame.py
@@ -6,9 +6,11 @@ import lib
 lib.initCountdown(5)
 
 games = {
-    "slimerancher":"steam://rungameid/433340","bloons":"steam://rungameid/960090",
+    "slimerancher":"steam://rungameid/433340",
+    "bloons":"steam://rungameid/960090",
     "tmnf":"\"C:\\Users\\Dario\\Documents\\Giochi\\TM Natitions\\Backup\\TMInfinity.exe\"",
-    "pheonix":"steam://rungameid/787480","cities":"steam://rungameid/255710",
+    "pheonix":"steam://rungameid/787480",
+    "cities":"steam://rungameid/255710",
     "prism":"\"C:\\Users\\Dario\\Documents\\Giochi\\Minecraft\\PrismLauncher\\prismlauncher.exe\"",
     "factorio":"steam://rungameid/427520",
     "autocad":"\"C:\\Program Files\\Autodesk\\AutoCAD 2023\\acad.exe\"  /product ACAD /language \"it-IT\"",
