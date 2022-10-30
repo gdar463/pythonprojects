5566a87cd8f357aa6a34f99ff8be9424c6e8d166
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-10-30 20:59:04 +0100

    Adding warning

--- screenshot.py
+++ screenshot.py
@@ -2,6 +2,7 @@ import time
 import random
 import pyautogui
 import lib
+import os
 
 lib.initCountdown(5)
 
@@ -12,3 +13,6 @@ while lib.checkIfProcRunning("PWAAT"):
     if random.randint(1,100) == 33:
         pyautogui.press("f12")
     time.sleep(1)
+
+print("No process found!")
+os.system("pause")
