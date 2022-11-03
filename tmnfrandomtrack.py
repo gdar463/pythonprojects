5e8de4d4a3df5634a429d6869e0a36ab9fe42ae2
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-11-03 17:05:00 +0100

    Resolving 6 number track

--- tmnfrandomtrack.py
+++ tmnfrandomtrack.py
@@ -1,12 +1,9 @@
-import lib
 import os
 import pyautogui
 import pyperclip
-import pywinauto
+import re
 import time
 import webbrowser
-
-lib.initCountdown(5)
 
 trackNum = input("How many tracks do you want?")
 
@@ -14,15 +11,20 @@ tracks = []
 
 for x in range(int(trackNum)):
     webbrowser.open("https://tmnf.exchange/trackrandom", 0, True)
-    time.sleep(0.25)
+    time.sleep(3)
     pyautogui.click(400,60)
     with pyautogui.hold("ctrl"):
         pyautogui.press(["a","c"])
     tracks.append(pyperclip.paste())
     with pyautogui.hold("ctrl"):
         pyautogui.press("w")
-    time.sleep(0.25)
+    time.sleep(0.5)
+
+track0 = re.sub("/D", "", tracks[0][-7:])
+webbrowser.open("https://tmnf.exchange/trackplay/" + track0, 1, False)
+tracks.pop(0)
 
 for x in tracks:
-    webbrowser.open(x)
+    x = re.sub("/D", "", x[-7:])
+    webbrowser.open("https://tmnf.exchange/trackplay/" + x, 0, False)
     os.system("pause")
