adc8fa5848e28e07e5976e7766cbb67e61787d89
gdar463 <63969957+gdar463@users.noreply.github.com>
2022-11-03 21:39:30 +0100

    Removing Unused Import

--- tmnfrandomtrack.py
+++ tmnfrandomtrack.py
@@ -1,7 +1,6 @@
 import os
 import pyautogui
 import pyperclip
-import re
 import time
 import webbrowser
 
@@ -23,10 +22,10 @@ for x in range(int(trackNum)):
         pyautogui.press("w")
     time.sleep(0.5)
 
-webbrowser.open("https://tmnf.exchange/trackplay/" + "".join(filter(str.isdigit, tracks[0])), 0, False)
+webbrowser.open("https://tmnf.exchange/trackplay/" + "".join(filter(str.isdigit, tracks[0][-7:])), 0, False)
 tracks.pop(0)
 os.system("pause")
 
 for x in tracks:
-    webbrowser.open("https://tmnf.exchange/trackplay/" + "".join(filter(str.isdigit, x)), 0, False)
+    webbrowser.open("https://tmnf.exchange/trackplay/" + "".join(filter(str.isdigit, x[-7:])), 0, False)
     os.system("pause")
