62274fbf845924d8ebae8419e01df5c8b7a69367
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-03-04 22:14:46 +0100

    Comment all hamming (can't solve fixes)

--- hamming.py
+++ hamming.py
@@ -1,60 +1,68 @@
-import random
+# import random
+# import time
 
-def onlyOne(input):
-    output = []
-    for x in input:
-        if x[1] == 1: output.append(x[0])
-    return output
+# def onlyOne(input):
+#     output = []
+#     for x in input:
+#         if x[1] == 1: output.append(x[0])
+#     return output
 
-times = int(input())
-startList = []
+# times = int(input())
+# startList = []
+# tiiis = 0
 
-for x in range(times):
-    startList.append(list((x, random.randint(0, 1))))
-print(str(startList)
-    #   .replace(", [", ",\n [")
-      )
+# for x in range(times):
+#     startList.append(list((x, random.randint(0, 1))))
+# print(str(startList)
+# #   .replace(", [", ",\n [")
+#     )
 
-onesList = onlyOne(startList)
+# onesList = onlyOne(startList)
 
-print(str(onesList)
-    #   4.replace(", [", ",\n [")
-      )
+# print(str(onesList)
+# #   .replace(", [", ",\n [")
+#     )
 
-error = onesList[0]
-for x in onesList:
-    if x == onesList[0]: pass
-    error = x ^ error
-print(error)
+# error = onesList[0]
+# for x in onesList:
+#     if x == onesList[0]: pass
+#     error = x ^ error
+# print(error)
 
-errorsBin = list(str(bin(error).split("b")[1]))
-print(errorsBin)
+# errorsBin = list(str(bin(error).split("b")[1]))
+# print(errorsBin)
+# print(len(errorsBin))
 
-for x in errorsBin:
-    if x == "1": 
-        index = "0b1"
-        for i in range(len(errorsBin) - 1):
-            index = index + "0"
-        print(index)
-        print(int(index, 2))
-        startList[int(index, 2)][1] = int(not startList[int(index, 2)][1])
-    errorsBin.pop(0)
+# for x in errorsBin:
+#     print(str(tiiis + 1))
+#     print("before " + str(errorsBin))
+#     errorsBin.pop(0)
+#     print(x)
+#     if x == "1": 
+#         index = "0b1"
+#         for i in range(len(errorsBin)):
+#             index = index + "0"
+#         print(index)
+#         print(int(index, 2))
+#         startList[int(index, 2)][1] = int(not startList[int(index, 2)][1])
+#     time.sleep(1)
+#     print("after " + str(errorsBin))
 
-print(str(startList)
-    #   .replace(", [", ",\n [")
-      )
+# print(str(startList)
+# #   .replace(", [", ",\n [")
+#     )
 
-onesList = onlyOne(startList)
+# onesList = onlyOne(startList)
 
-print(str(onesList)
-    #   .replace(", [", ",\n [")
-      )
+# print(str(onesList)
+# #   .replace(", [", ",\n [")
+#     )
 
-error = onesList[0]
-for x in onesList:
-    if x == onesList[0]: pass
-    error = x ^ error
-print(error)
+# error = onesList[0]
+# for x in onesList:
+#     if x == onesList[0]: pass
+#     error = x ^ error
+# print(error)
 
-errorsBin = list(str(bin(error).split("b")[1]))
-print(errorsBin)
+# errorsBin = list(str(bin(error).split("b")[1]))
+# print(errorsBin)
